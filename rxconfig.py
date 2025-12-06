4dd5080f21341881b1ebe015abcdb4f870c7f955
Ernesto Crespo <ecrespo@protonmail.com>
2025-12-06 19:45:39 -0400

    chore: Set state manager mode to in-memory for Vercel deployment

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="web",
     favicon="👨‍🔬",
+    state_manager_mode="memory",  # Use in-memory state manager for Vercel deployment
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
